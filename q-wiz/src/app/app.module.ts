edf0d6a2cbf3c685edf3d54ed73f01c020dfaf1f
remiimartinnn <32273204+remiimartinnn@users.noreply.github.com>
2022-01-03 17:29:17 -0500

    frontend push
    added components & assets

--- q-wiz/src/app/app.module.ts
+++ q-wiz/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CreateQuizComponent } from './create-quiz/create-quiz.component';
+import { CreateQuestionAnswerComponent } from './create-question-answer/create-question-answer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TakeQuizComponent } from './take-quiz/take-quiz.component';
+import { ResultComponent } from './result/result.component';
+import { CreateTagsComponent } from './create-tags/create-tags.component';
+import { FilterByTagsComponent } from './filter-by-tags/filter-by-tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateQuizComponent,
+    CreateQuestionAnswerComponent,
+    NavbarComponent,
+    TakeQuizComponent,
+    ResultComponent,
+    CreateTagsComponent,
+    FilterByTagsComponent
   ],
   imports: [
     BrowserModule,
