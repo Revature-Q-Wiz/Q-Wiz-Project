e50eee46b1d2becab9ef31f3b0284bacccae4e70
remiimartinnn <32273204+remiimartinnn@users.noreply.github.com>
2022-01-04 12:04:50 -0500

    all routes are working!
    .

--- q-wiz/src/app/app.module.ts
+++ q-wiz/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -13,6 +14,8 @@ import { ResultComponent } from './result/result.component';
 import { CreateTagsComponent } from './create-tags/create-tags.component';
 import { FilterByTagsComponent } from './filter-by-tags/filter-by-tags.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from 'src/routes';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,8 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
