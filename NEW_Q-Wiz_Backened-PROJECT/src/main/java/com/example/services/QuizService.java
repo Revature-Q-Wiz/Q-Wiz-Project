6eebadcc20f1b342b383883edeee6afb075930b2
remiimartinnn <32273204+remiimartinnn@users.noreply.github.com>
2022-01-11 14:36:24 -0500

    edits to quiz controller, service, and service impl

--- NEW_Q-Wiz_Backened-PROJECT/src/main/java/com/example/services/QuizService.java
+++ NEW_Q-Wiz_Backened-PROJECT/src/main/java/com/example/services/QuizService.java
@@ -1,5 +1,12 @@
 package com.example.services;
 
+import com.example.entity.Employee;
+import com.example.entity.Quiz;
+
+import org.springframework.stereotype.Service;
+
 public interface QuizService {
-    
+    public List<Quiz> getAllQuizzes();
+    public Quiz getQuizById(int quiz_id);
+    public Quiz addQuiz(Quiz quiz);
 }
