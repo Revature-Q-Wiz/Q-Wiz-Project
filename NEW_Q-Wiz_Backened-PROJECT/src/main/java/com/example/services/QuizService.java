eaed57c7ffd9fbd07e255f0693dccf86c454b79e
carlosleongonzalez <juniorleon21@hotmail.com>
2022-01-11 10:00:04 -0800

    created quiz service, quiz service implementation and quiz controller
    I developed a service, service implementation and controller for quiz

---
+++ NEW_Q-Wiz_Backened-PROJECT/src/main/java/com/example/services/QuizService.java
@@ -0,0 +1,18 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Quiz;
+
+@Service
+public interface QuizService {
+	
+	public List<Quiz> getAllQuizzes();
+    public Quiz getQuizById(int id);
+    public Quiz addQuiz(Quiz quiz);
+    public void deleteQuiz(int id);
+    public Quiz updateQuiz(int id, Quiz quiz);
+
+}
