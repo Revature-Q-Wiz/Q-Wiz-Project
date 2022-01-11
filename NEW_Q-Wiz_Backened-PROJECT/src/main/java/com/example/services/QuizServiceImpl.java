e040ae7793caa08709800bb0ec6d152251e2f044
remiimartinnn <32273204+remiimartinnn@users.noreply.github.com>
2022-01-11 16:37:22 -0500

    deleted old backend and fixed quiz service impl

--- NEW_Q-Wiz_Backened-PROJECT/src/main/java/com/example/services/QuizServiceImpl.java
+++ NEW_Q-Wiz_Backened-PROJECT/src/main/java/com/example/services/QuizServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class QuizServiceImpl {
+public class QuizServiceImpl implements QuizService{
     
     @Autowired
     private QuizRepository repository;
